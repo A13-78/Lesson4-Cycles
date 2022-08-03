232ca631d7d779641483d5c2675c7dcd42221211
Urok1 <a13-78@mail.ru>
2022-08-03 14:49:56 +0400

    Выполненное домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(" Привет, Java!");
+
+        // Задание 1
+        int counter = 1;
+        while (counter <= 10) {
+            System.out.print(counter + " ");
+            counter++;
+        }
+        System.out.println();
+        for (int i = counter - 1; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Задание 2
+        int monthDays = 31;
+        for (int i = 1; i < monthDays; i = i + 7) {
+            System.out.println("Сегодня пятница, " + i + " - е число. Необходимо подготовить отчет.");
+        }
+        System.out.println();
+
+        // Задание 3
+        int currentYear = 2022;
+        int elementaryYear = currentYear - 200;
+        int futureYear = currentYear + 100;
+        for(int i = 0; i < futureYear; i = i + 79) {
+            if (i > elementaryYear){
+                System.out.println(i);
+            }
+        }
     }
 }
